fdae2940b895ea773b2b6af05959b8d83bc33ea2
Brittany <bolivthom@gmail.com>
2019-01-30 23:27:30 +0000

    routes created

--- app/views.py
+++ app/views.py
@@ -5,8 +5,10 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, session
 from flask_login import login_user, logout_user, current_user, login_required
 from forms import LoginForm
 from models import UserProfile
@@ -27,27 +29,35 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    """Render a secure page on our website that only logged in users can access."""
+    if not session.get('logged_in'):
+        flash('Login Required', 'danger')
+        return redirect(url_for("login"))
+    return render_template('secure_page.html')
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
+    if request.method == "POST" and form.validate_on_submit():
         if form.username.data:
-            # Get the username and password values from the form.
-
-            # using your model, query database for a user based on the username
-            # and password submitted
-            # store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method.
-
-            # get user id, load into session
+            username = form.username.data
+            password = form.password.data
+            user = UserProfile.query.filter_by(username=username, password=password).first()
             login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            session['logged_in'] = True
+            flash('Logged in successfully.', 'success')
+            return redirect(url_for("secure_page"))
     return render_template("login.html", form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out', 'success')
+    return redirect(url_for('home'))
 
 
 # user_loader callback. This callback is used to reload the user object from
